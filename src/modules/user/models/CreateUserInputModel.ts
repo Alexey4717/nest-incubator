7438e3dd60efb7e4b272651d25284bf0ee007e9f
Alexey4717 <lehabourne@gmail.com>
2023-05-07 23:57:39 +1000

    fix bugs 8

--- src/modules/user/models/CreateUserInputModel.ts
+++ src/modules/user/models/CreateUserInputModel.ts
@@ -1,16 +1,16 @@
 export type CreateUserInputModel = {
-    /**
-     * Set user`s login. Required. MaxLength: 10, minLength: 3.
-     */
-    login: string
+  /**
+   * Set user`s login. Required. MaxLength: 10, minLength: 3.
+   */
+  login: string;
 
-    /**
-     * Password of created user. Required. MaxLength: 20, minLength: 6.
-     */
-    password: string
+  /**
+   * Password of created user. Required. MaxLength: 20, minLength: 6.
+   */
+  password: string;
 
-    /**
-     * Set user`s email, required.
-     */
-    email: string
-}
+  /**
+   * Set user`s email, required.
+   */
+  email: string;
+};
