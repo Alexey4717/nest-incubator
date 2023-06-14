4b3c42be735597bdcd73691e800a296d02fc2147
Alexey4717 <lehabourne@gmail.com>
2023-06-15 02:48:08 +1000

    start h14 - 3

--- src/modules/testing/infrastructure/testing.repository.ts
+++ src/modules/testing/infrastructure/testing.repository.ts
@@ -5,6 +5,7 @@ import { Post, PostDocument } from '../../post/models/Post.schema';
 import { Blog, BlogDocument } from '../../blog/models/Blog.schema';
 import { User, UserDocument } from '../../user/models/User.schema';
 import { Comment, CommentDocument } from '../../comment/models/Comment.schema';
+import { Session, SessionDocument } from '../../session/models/session.schema';
 
 @Injectable()
 export class TestingRepository {
@@ -12,6 +13,8 @@ export class TestingRepository {
     @InjectModel(Blog.name) private readonly BlogModel: Model<BlogDocument>,
     @InjectModel(Post.name) private readonly PostModel: Model<PostDocument>,
     @InjectModel(User.name) private readonly UserModel: Model<UserDocument>,
+    @InjectModel(Session.name)
+    private readonly SessionModel: Model<SessionDocument>,
     @InjectModel(Comment.name)
     private readonly CommentModel: Model<CommentDocument>,
   ) {}
@@ -19,12 +22,15 @@ export class TestingRepository {
   async deleteAllData(): Promise<boolean> {
     try {
       await this.UserModel.deleteMany({});
+      await this.SessionModel.deleteMany({});
       await this.BlogModel.deleteMany({});
       await this.PostModel.deleteMany({});
       await this.CommentModel.deleteMany({});
       return true;
     } catch (error) {
-      console.log(`postsRepository.createPost error is occurred: ${error}`);
+      console.log(
+        `TestingRepository.deleteAllData error is occurred: ${error}`,
+      );
       return false;
     }
   }
